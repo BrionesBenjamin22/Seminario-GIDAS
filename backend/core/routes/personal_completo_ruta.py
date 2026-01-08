976204a15cf8765959ab2989e9ab2b6c31f5816a
Benjamin551 <benjaminbrionessorarrain@alu.frlp.utn.edu.ar>
2026-01-08 01:40:20 -0300

    Comentarios agregados al archivo de rutas

--- backend/core/routes/personal_completo_ruta.py
+++ backend/core/routes/personal_completo_ruta.py
@@ -2,10 +2,14 @@
 from flask import Blueprint
 from core.controllers.personal_completo_controller import PersonalCompletoController
 
+
+# Este Endpoint lista todo el personal (Personal, Becarios, Investigadores) en una sola llamada, simplificando la obtención de datos completos en las cartas del frontend.
+
+
 personal_completo_bp = Blueprint(
     "personal_completo",
     __name__,
-    url_prefix="/personal-completo"
+    url_prefix="/personal-all"
 )
 
 @personal_completo_bp.route("/", methods=["GET"])
